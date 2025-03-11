13fae30a7afe76610aeea1d847c439651adaa91b
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2025-03-11 10:31:19 +0200

    refractor goals get, post endpoints and serializer

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Goal
+from django.utils import timezone
 
 class GoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goal
         fields = ['id', 'user', 'description', 'start_date', 'deadline', 'is_completed']
+
+
+    def validate_deadlite(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Deadling cannot be before today")
+        return value
--- server/api/views.py
+++ server/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.utils import timezone
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,20 +6,17 @@ from rest_framework import status
 
 from .models import Goal
 from .serializers import GoalSerializer
-from users.models import User
+
 # Create your views here.
 
 class Goals(APIView):
     def get(self, request):
-        user = User.objects.get(username="nuzo_admin")
-        goals = Goal.objects.filter(user=user)
+        goals = Goal.objects.filter(user=request.user)
 
-        serializer = GoalSerializer(goals)
+        serializer = GoalSerializer(goals, many=True)
         return Response({ "goals": serializer.data}, status=status.HTTP_200_OK)
     
     def post(self, request):
-        user = User.objects.get(username=("nuzo_admin"))
-
         if not request.user.is_authenticated:
             return Response({ "message": "You must be signed in to use this feature" }, status=status.HTTP_401_UNAUTHORIZED)
         
@@ -28,12 +25,20 @@ class Goals(APIView):
 
         if not description or not deadline:
             return Response({ "message": "Description and deadline are required." },status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            deadline = timezone.make_aware(deadline)
+        except ValueError:
+            return Response({ "message": "Invalid deadline format." }, status=status.HTTP_400_BAD_REQUEST)
 
-        new_goal = Goal.objects.create(
-            user=user,
-            description=description,
-            deadline=deadline
-        )
+        try:
+            new_goal = Goal.objects.create(
+                user=request.user,
+                description=description,
+                deadline=deadline
+            )
+        except Exception as e:
+             return Response({"message": f"An error occurred while creating the goal: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         serializer = GoalSerializer(new_goal)
 
