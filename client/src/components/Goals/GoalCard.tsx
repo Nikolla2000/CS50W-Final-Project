337490744e27771ea27dc325587685bcd4ce44c9
Nikola Uzunov <109785756+Nikolla2000@users.noreply.github.com>
2025-03-19 14:05:11 +0200

    add shadow to a button component

--- client/src/components/Goals/GoalCard.tsx
+++ client/src/components/Goals/GoalCard.tsx
@@ -7,22 +7,29 @@ export default function GoalCard({ goal }: { goal: GoalsData }) {
   const formattedDeadline = formatDistanceToNow(new Date(goal.deadline), { addSuffix: true });
 
   return (
-    <Card sx={{ maxWidth: 345, margin: '1rem', borderRadius: '12px', boxShadow: 3 }}>
-      <CardContent>
-        <Typography variant="h5" component="div" sx={{ fontWeight: 'bold', color: '#333' }}>
-          {goal.description}
-        </Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
-          <Typography variant="body2" color="text.secondary">
-            Deadline: {formattedDeadline}
-          </Typography>
-          {goal.is_completed ? (
-            <Chip label="Completed" color="success" sx={{ width: 'fit-content' }} />
-          ) : (
-            <Chip label="Pending" color="warning" sx={{ width: 'fit-content' }} />
-          )}
-        </Box>
-      </CardContent>
-    </Card>
+    // <Card sx={{ maxWidth: 345, margin: '1rem', borderRadius: '12px', boxShadow: 3}} >
+    //   <CardContent>
+    //     <Typography variant="h5" component="div" sx={{ fontWeight: 'bold', color: '#333' }}>
+    //       {goal.description}
+    //     </Typography>
+    //     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+    //       <Typography variant="body2" color="text.secondary">
+    //         Deadline: {formattedDeadline}
+    //       </Typography>
+    //       {goal.is_completed ? (
+    //         <Chip label="Completed" color="success" sx={{ width: 'fit-content' }} />
+    //       ) : (
+    //         <Chip label="Pending" color="warning" sx={{ width: 'fit-content' }} />
+    //       )}
+    //     </Box>
+    //   </CardContent>
+    // </Card>
+    <div className="goal-card-wrapper">
+        <h5>
+            {goal.description}
+        </h5>
+        <p>Deadlinbg : {formattedDeadline}</p>
+        <div className='goal-details'></div>
+    </div>
   );
 }
