9b02d9f23e50035b7aac2a8bb86b59c651ffec44
Nikola Uzunov <nikollla2000@abv.bg>
2025-03-26 22:50:44 +0200

    tasks api routes in urls.py

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import Goals
+from .tasks_views import Tasks, TaskDetailView
 
 app_name = 'api'
 
 urlpatterns = [
     path("goals/", Goals.as_view(), name="goals"),
     path("goals/<int:goal_id>/complete/", Goals.as_view(), name="complete_goal"),
+
+    path("tasks/", Tasks.as_view(), name="tasks"),
+    path("tasks/<int:task_id>/", TaskDetailView.as_view(), name="task-detail"),
 ]
